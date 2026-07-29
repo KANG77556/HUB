fdcea7d5884e8dd1570dfe27e25fc61b5c271990
KANG77556 <204034@gmail.com>
2026-07-29 09:25:15 +0900

    feat(server-manager): wire setup window to view model

---
+++ apps/server-manager/SchoolWorkHub.ServerManager/MainWindow.xaml.cs
@@ -0,0 +1,24 @@
+using System.Windows;
+using System.Windows.Controls;
+using SchoolWorkHub.ServerManager.ViewModels;
+
+namespace SchoolWorkHub.ServerManager;
+
+public partial class MainWindow : Window
+{
+    private readonly MainWindowViewModel _viewModel = new();
+
+    public MainWindow()
+    {
+        InitializeComponent();
+        DataContext = _viewModel;
+    }
+
+    private void AdminPassword_OnPasswordChanged(object sender, RoutedEventArgs e)
+    {
+        if (sender is PasswordBox passwordBox)
+        {
+            _viewModel.SetAdminPassword(passwordBox.Password);
+        }
+    }
+}
