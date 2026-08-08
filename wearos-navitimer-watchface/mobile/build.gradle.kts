730497e4ef13fa21233498821749341fd8eeaf89
KANG77556 <204034@gmail.com>
2026-08-08 22:18:52 +0900

    build: configure mobile Kotlin app

--- wearos-navitimer-watchface/mobile/build.gradle.kts
+++ wearos-navitimer-watchface/mobile/build.gradle.kts
@@ -1,4 +1,7 @@
-plugins { id("com.android.application") }
+plugins {
+    id("com.android.application")
+    id("org.jetbrains.kotlin.android")
+}
 
 android {
     namespace = "com.kang77556.premiumwatchface.mobile"
@@ -13,5 +16,9 @@ android {
 }
 
 dependencies {
+    implementation("androidx.appcompat:appcompat:1.7.0")
+    implementation("androidx.core:core-ktx:1.15.0")
+    implementation("com.google.android.material:material:1.12.0")
     implementation("com.google.android.gms:play-services-wearable:19.0.0")
+    testImplementation("junit:junit:4.13.2")
 }
