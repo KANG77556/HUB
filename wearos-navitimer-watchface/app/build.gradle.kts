e48890d03ef08d742dbe173193f5453125740840
KANG77556 <204034@gmail.com>
2026-08-08 21:36:39 +0900

    Add Wear OS app module

---
+++ wearos-navitimer-watchface/app/build.gradle.kts
@@ -0,0 +1,13 @@
+plugins { id("com.android.application") }
+
+android {
+    namespace = "com.kang77556.premiumwatchface"
+    compileSdk = 35
+    defaultConfig {
+        applicationId = "com.kang77556.premiumwatchface"
+        minSdk = 30
+        targetSdk = 35
+        versionCode = 1
+        versionName = "1.0"
+    }
+}
