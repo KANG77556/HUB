9dca0dec84c498828e1e9a0f9387ce8499a8c61a
KANG77556 <204034@gmail.com>
2026-08-09 17:30:23 +0900

    fix: configure wear module as WFF v1 resource package

--- wearos-navitimer-watchface/wear/build.gradle.kts
+++ wearos-navitimer-watchface/wear/build.gradle.kts
@@ -1,6 +1,5 @@
 plugins {
     id("com.android.application")
-    id("org.jetbrains.kotlin.android")
 }
 
 android {
@@ -8,19 +7,13 @@ android {
     compileSdk = 35
     defaultConfig {
         applicationId = "com.kang77556.premiumwatchface.wear"
-        minSdk = 30
+        minSdk = 33
         targetSdk = 35
-        versionCode = 1
-        versionName = "1.0"
-    }
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_17
-        targetCompatibility = JavaVersion.VERSION_17
+        versionCode = 2
+        versionName = "2.0"
     }
 }
 
 dependencies {
-    implementation("androidx.core:core-ktx:1.15.0")
-    implementation("com.google.android.gms:play-services-wearable:19.0.0")
     testImplementation("junit:junit:4.13.2")
 }
