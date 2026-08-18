58fc4f1553355ca4072f1a441f9cc9a49eb9cd9f
KANG77556 <204034@gmail.com>
2026-08-18 18:22:38 +0900

    test: compile viewer strategy in domain suite

--- android-source/scripts/test-domain.sh
+++ android-source/scripts/test-domain.sh
@@ -6,6 +6,7 @@ kotlinc \
   "$ROOT/app/src/main/java/kr/co/alldocuments/domain/DocumentModels.kt" \
   "$ROOT/app/src/main/java/kr/co/alldocuments/domain/DocumentClassifier.kt" \
   "$ROOT/app/src/main/java/kr/co/alldocuments/domain/DocumentMimeResolver.kt" \
+  "$ROOT/app/src/main/java/kr/co/alldocuments/domain/DocumentViewerStrategy.kt" \
   "$ROOT/domain-tests/DomainTest.kt" \
   -include-runtime -d "$OUT"
 java -jar "$OUT"
