09ef3f8caf79460330bac574392d4bf5b443c7a8
KANG77556 <204034@gmail.com>
2026-08-18 16:33:03 +0900

    test: include MIME resolver in domain tests

--- android-source/scripts/test-domain.sh
+++ android-source/scripts/test-domain.sh
@@ -5,6 +5,7 @@ OUT="${TMPDIR:-/tmp}/all-documents-domain-tests.jar"
 kotlinc \
   "$ROOT/app/src/main/java/kr/co/alldocuments/domain/DocumentModels.kt" \
   "$ROOT/app/src/main/java/kr/co/alldocuments/domain/DocumentClassifier.kt" \
+  "$ROOT/app/src/main/java/kr/co/alldocuments/domain/DocumentMimeResolver.kt" \
   "$ROOT/domain-tests/DomainTest.kt" \
   -include-runtime -d "$OUT"
 java -jar "$OUT"
